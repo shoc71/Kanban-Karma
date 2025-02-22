e36b5b91061b6358d2aa3b55ebfc4f1ff8032809
shoc71 <supersonicwind6@gmail.com>
2025-02-22 18:51:26 -0500

    deployment test push

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth';
 import apiRoutes from './routes/index'; // Import the secured API routes
 import { errorHandler } from './middleware/errorHandler';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../../client/build')));
+
+// Catch-all: for any route not matching API, serve React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+});
 
 // Public Routes & Protected API Routes
 app.use('/api/auth', authRoutes);
