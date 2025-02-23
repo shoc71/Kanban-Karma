c7f7a81d6b86fb217cd391de7ac28d0138f318b8
shoc71 <supersonicwind6@gmail.com>
2025-02-22 23:47:52 -0500

    risky push 8

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,18 +17,17 @@ app.use(express.json());
 // const CLIENT_URL = process.env.CLIENT_URL || `http://localhost:5173`;
 app.use(cors({ origin: `http://localhost:${PORT}`, credentials: true }));
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '../../client/dist')));
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 // Public Routes & Protected API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/auth', apiRoutes);
 
 // Catch-all: for any route not matching API, serve React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
-});
-
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
+// });
 
 app.use(errorHandler);
 
